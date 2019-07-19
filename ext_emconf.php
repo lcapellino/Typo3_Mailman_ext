0eb3d23a2f4ae0cf72e9d42010401f9ba9d2466c
lcapellino <lucas_capellino@web.de>
2019-07-19 15:37:21 +0200

    composer error fixed

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,5 +22,5 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'lucas capellino, mathias kupferschmid',
 	'author_email' => 'lu721cap@htwg-konstanz.de',
 	'author_company' => 'htwg',
-	'version' => '1.1.2',
+	'version' => '1.1.3',
 ];
